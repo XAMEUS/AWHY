f426f705ea6bc7e047ba4be24dc9325d19e37811
Maxime <maxime.gourgoulhon@gmail.com>
2017-12-05 09:24:29 +0100

    up

--- src/TestTransaction.java
+++ src/TestTransaction.java
@@ -1,4 +1,5 @@
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.Scanner;
 
@@ -10,7 +11,12 @@ public class TestTransaction {
 		Dialog d = new Dialog();
 		d.connect();
 		Client c = new Client(1, "G", "Maxime", "individuel", "1 rue X", "m@g", "06", 2017);
-		c.insertSQL(d.getConnection());
+		// c.insertSQL(d.getConnection());
+		String sqlIdentifier = "select idClient.NEXTVAL from Client";
+		PreparedStatement pst = d.getConnection().prepareStatement(sqlIdentifier);
+		ResultSet rs = pst.executeQuery();
+		if (rs.next())
+			System.out.println("id :" + rs.getLong(1));
 		Scanner sc = new Scanner(System.in);
 		System.out.print("Press [ENTER] to continue...");
 		sc.nextLine();
