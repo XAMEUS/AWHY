6bc4bdb5ac0e19354c4e5f0f745d72dbe5614a91
Julie Saouli <Julie.Saouli@ensimag.grenoble-inp.fr>
2017-12-05 09:24:54 +0100

    CircuitTable

---
+++ src/org/awhy/ui/tables/CircuitTable.java
@@ -0,0 +1,44 @@
+package org.awhy.ui.tables;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import org.awhy.core.objects.Circuit;
+
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.cell.PropertyValueFactory;
+
+public class CircuitTable extends Table<Circuit>{
+	
+	public CircuitTable(ResultSet res) throws SQLException {
+		super(new Circuit(), res);
+		
+		TableColumn<Circuit, String> idCircuitCol = new TableColumn<Circuit, String>("ID");
+		idCircuitCol.setCellValueFactory(new PropertyValueFactory<Circuit, String>("idCircuit"));
+		TableColumn<Circuit, String> descriptifCol = new TableColumn<Circuit, String>("Descriptif");
+		descriptifCol.setCellValueFactory(new PropertyValueFactory<Circuit, String>("descriptif"));
+		TableColumn<Circuit, String> villeDepartCol = new TableColumn<Circuit, String>("Ville de départ");
+		villeDepartCol.setCellValueFactory(new PropertyValueFactory<Circuit, String>("villeDepart"));
+		TableColumn<Circuit, String> paysDepartCol = new TableColumn<Circuit, String>("Pays de départ");
+		paysDepartCol.setCellValueFactory(new PropertyValueFactory<Circuit, String>("paysDepart"));
+		TableColumn<Circuit, String> villeArriveeCol = new TableColumn<Circuit, String>("Ville d'arrivée");
+		villeArriveeCol.setCellValueFactory(new PropertyValueFactory<Circuit, String>("villeArrivee"));
+		TableColumn<Circuit, String> paysArriveeCol = new TableColumn<Circuit, String>("Pays d'arrivée");
+		paysArriveeCol.setCellValueFactory(new PropertyValueFactory<Circuit, String>("paysArrivee"));
+		TableColumn<Circuit, String> nbJoursTotalCol = new TableColumn<Circuit, String>("Jours");
+		nbJoursTotalCol.setCellValueFactory(new PropertyValueFactory<Circuit, String>("nbJoursTotal"));
+		TableColumn<Circuit, String> prixCircuitCol = new TableColumn<Circuit, String>("Prix");
+		prixCircuitCol.setCellValueFactory(new PropertyValueFactory<Circuit, String>("prixCircuit"));
+		
+		this.setItems(this.data);
+		this.getColumns().add(idCircuitCol);
+		this.getColumns().add(descriptifCol);
+		this.getColumns().add(villeDepartCol);
+		this.getColumns().add(paysDepartCol);
+		this.getColumns().add(villeArriveeCol);
+		this.getColumns().add(paysArriveeCol);
+		this.getColumns().add(nbJoursTotalCol);
+		this.getColumns().add(prixCircuitCol);
+	}
+
+}
