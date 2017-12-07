760aa59e92c6240dd8059d87cabc8e265269fdf2
Julie Saouli <Julie.Saouli@ensimag.grenoble-inp.fr>
2017-12-07 19:10:40 +0100

    reserve circuit jusqu'a date mais bug au commit

--- src/org/awhy/ui/popup/PopupCircuit.java
+++ src/org/awhy/ui/popup/PopupCircuit.java
@@ -1,9 +1,13 @@
 package org.awhy.ui.popup;
 
+import java.sql.Date;
 import java.sql.SQLException;
 import java.util.Optional;
 
 import org.awhy.core.objects.Circuit;
+import org.awhy.core.objects.DateCircuit;
+import org.awhy.core.objects.ReserveCircuit;
+import org.awhy.core.objects.ReserveHotel;
 import org.awhy.core.objects.Simulation;
 import org.awhy.ui.Controller;
 import org.awhy.ui.pane.CircuitPane;
@@ -16,6 +20,8 @@ import javafx.scene.control.ButtonType;
 import javafx.scene.control.Dialog;
 import javafx.scene.control.DialogEvent;
 import javafx.scene.control.Label;
+import javafx.scene.control.TableRow;
+import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
 import javafx.scene.control.ButtonBar.ButtonData;
 import javafx.scene.layout.GridPane;
@@ -52,10 +58,26 @@ public class PopupCircuit {
 		Optional<ButtonType> result = dialog.showAndWait();
 
 		if (result.isPresent()) {
+			
 			try {
 				Controller.container.setTableView(new DateCircuitTable(
 						Controller.executeQuery("select * from dateCircuit where idCircuit = " + data.getIdCircuit())));
-				//TODO: reserver date
+				//TODO: reserver date, new popup ?
+				TableView<DateCircuit> v = (TableView<DateCircuit>) (Controller.tableView);
+				v.setRowFactory(tv -> {
+					TableRow<DateCircuit> row = new TableRow<>();
+					row.setOnMouseClicked(event -> {
+						if (event.getClickCount() == 2 && (!row.isEmpty())) {
+							DateCircuit rowData = row.getItem();
+							// tp.setText ne fonctionne pas ?
+							tp.object = new ReserveCircuit(data.getIdCircuit(), rowData.getDateDepartCircuit(), 
+									s.getNumDossier(), Integer.valueOf(nbPersonnes.getText()));
+							System.out.println(rowData.getDateDepartCircuit());
+							// TODO : revenir aux circuits
+						}
+					});
+					return row;
+				});
 			} catch (SQLException e) {
 				e.printStackTrace();
 			}
