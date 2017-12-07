f51d06ba756e90d468dadd6e655f2672ae329547
Maxime <maxime.gourgoulhon@gmail.com>
2017-12-07 10:48:42 +0100

    client auto id generation

--- src/TestTransaction.java
+++ src/TestTransaction.java
@@ -10,15 +10,17 @@ public class TestTransaction {
 	public static void main(String[] args) throws SQLException {
 		Dialog d = new Dialog();
 		d.connect();
-//		Client c = new Client(1, "G", "Pierre", "individuel", "1 rue X", "m@g", "06", 2017);
-		ResultSet res = d.executeQuery("select * from client where idclient='1'");
-		Client c = null;
-		if (res.next())
-			c = (Client) new Client().createFromSQL(res);
-		System.out.println(c.getNomClient());
-		c.setNomClient("<3");
-		c.updateSQL(d.getConnection());
-//		c.insertSQL(d.getConnection());
+		Client c = new Client(d, "Pierrounet <3", "Pierre", "individuel", "1 rue X", "m@g", "06", 2017);
+//		ResultSet res = d.executeQuery("select idclient.nextval from client");
+//		Client c = null;
+//		if (res.next())
+//			System.out.println(res.getLong(1));
+//			c = (Client) new Client().createFromSQL(res);
+		System.out.println(c.getIdClient());
+//		System.out.println(c.getNomClient());
+//		c.setNomClient("<3");
+//		c.updateSQL(d.getConnection());
+		c.insertSQL(d.getConnection());
 //		String sqlIdentifier = "select idClient.NEXTVAL from Client";
 //		PreparedStatement pst = d.getConnection().prepareStatement(sqlIdentifier);
 //		ResultSet rs = pst.executeQuery();
