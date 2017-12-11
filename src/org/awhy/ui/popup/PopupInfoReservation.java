6c95a64c22698b1bc1f6e771fa39a21800a1be63
Julie Saouli <Julie.Saouli@ensimag.grenoble-inp.fr>
2017-12-11 12:32:48 +0100

    remove test palce dans infoReserve

--- src/org/awhy/ui/popup/PopupInfoReservation.java
+++ src/org/awhy/ui/popup/PopupInfoReservation.java
@@ -148,56 +148,7 @@ public class PopupInfoReservation {
 
 		Text argent = new Text("Coût total: " + cout + "€");
 		grid.add(argent, 1, 1);
-
-		// vérifier (nombre places circuits, nombre places hotels, dates)
-		boolean possible = true;
-		query = "SELECT nbPersonnesCircuit, idCircuit, dateDepartCircuit FROM ReserveCircuit WHERE numDossier=?";
-		pS = c.prepareStatement(query);
-		pS.setInt(1, numDossier);
-		res = pS.executeQuery();
-		while (res.next()) {
-			if (!possible)
-				break;
-			String check = "select (dc.nbPersonnes - sum(rcd.nbPersonnesCircuit)) as nbPlaces from DateCircuit dc, (select idCircuit, dateDepartCircuit, nbPersonnesCircuit from ReserveCircuit rc, Reservation r where r.numDossier = rc.numDossier and rc.idCircuit = ? and rc.dateDepartCircuit = ?) rcd where dc.idCircuit = rcd.idCircuit and dc.dateDepartCircuit = rcd.dateDepartCircuit group by dc.idCircuit, dc.dateDepartCircuit, dc.nbPersonnes";
-			PreparedStatement cPS = c.prepareStatement(check);
-			cPS.setString(1, res.getString(2));
-			cPS.setDate(2, res.getDate(3));
-			System.out.println(check);
-			ResultSet cRes = cPS.executeQuery();
-			while (cRes.next())
-				if (cRes.getInt(1) < res.getInt(1))
-					possible = false;
-			cPS.close();
-		}
-		pS.close();
-
-		query = "SELECT nomHotel, ville, pays, nbChambresReservees, dateDepartHotel, dateArriveeHotel FROM ReserveHotel WHERE numDossier=?";
-		pS = c.prepareStatement(query);
-		pS.setInt(1, numDossier);
-		res = pS.executeQuery();
-		while (res.next()) {
-			if (!possible)
-				break;
-			Date curr = new Date(res.getDate(5).getTime());
-			while (res.getDate(6).after(curr)) {
-				if (!possible)
-					break;
-				String check = "select (h.nbChambresTotal - sum(rhr.nbChambresReservees)) from (select nomHotel, ville, pays, nbChambresTotal from Hotel) h, (select rh.nomHotel, rh.ville, rh.pays, rh.nbChambresReservees from ReserveHotel rh, Reservation r where rh.numDossier = r.numDossier and rh.nomHotel = ? and rh.ville = ? and rh.pays = ? and (dateDepartHotel <= ? and dateArriveeHotel > ?)) rhr where h.nomHotel = rhr.nomHotel and h.ville = rhr.ville and h.pays = rhr.pays group by h.nomHotel, h.ville, h.pays, h.nbChambresTotal";
-				PreparedStatement cPS = c.prepareStatement(check);
-				cPS.setDate(4, curr);
-				cPS.setDate(5, curr);
-				cPS.setString(1, res.getString(1));
-				cPS.setString(2, res.getString(2));
-				cPS.setString(3, res.getString(3));
-				ResultSet cRes = cPS.executeQuery();
-				while (cRes.next())
-					if (res.getInt(4) > cRes.getInt(1))
-						possible = false;
-				cPS.close();
-				curr = new Date(curr.getTime() + ((long) 1000) * 60 * 60 * 24);
-			}
-		}
-		pS.close();
+		
 
 		final ReserveCircuitTable recapCircuit = new ReserveCircuitTable(
 				Controller.executeQuery("select * from reservecircuit where numDossier = '" + numDossier + "'"));
