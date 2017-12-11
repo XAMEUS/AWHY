c849ccdcf051805346f0a997c04624c0b3e7693e
Maxime <maxime.gourgoulhon@gmail.com>
2017-12-11 11:28:26 +0100

    pane infos

--- src/org/awhy/ui/pane/TPane.java
+++ src/org/awhy/ui/pane/TPane.java
@@ -4,8 +4,21 @@ import java.util.ArrayList;
 
 import org.awhy.core.objects.Object;
 
+import javafx.scene.Node;
 import javafx.scene.control.TitledPane;
+import javafx.scene.layout.GridPane;
 
 public class TPane extends TitledPane {
 	public ArrayList<Object> objects = new ArrayList<>();
+	private GridPane content;
+	private int i = 0;
+	
+	public TPane() {
+		content = new GridPane();
+		this.setContent(this.content);
+	}
+	
+	public void add(Node e) {
+		this.content.add(e, 0, i++);
+	}
 }
